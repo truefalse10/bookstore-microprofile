472e4daac696ee6dce050d7e71cfe03dbe91b338
Sebastian Boolean <truefalse@posteo.de>
2019-08-20 15:42:52 +0200

    adding eclipse.microprofile.config for configuration

--- src/main/java/com/truefalse01/PingResource.java
+++ src/main/java/com/truefalse01/PingResource.java
@@ -1,14 +1,21 @@
 package com.truefalse01;
 
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 @Path("ping")
 public class PingResource {
 
+    @Inject
+    @ConfigProperty(name = "VERSION")
+    String VERSION;
+
     @GET
     public String ping() {
-        return "Enjoy Java EE 8 with MicroProfile";
+        return "Enjoy Java EE 8 with MicroProfile v." + VERSION + "!";
     }
 
 }
