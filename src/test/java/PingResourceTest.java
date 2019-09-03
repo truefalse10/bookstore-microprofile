d341d4b28451334490f7ecf2def0ecede13596b1
Sebastian Boolean <truefalse@posteo.de>
2019-09-03 09:04:35 +0200

    update todo

--- src/test/java/PingResourceTest.java
+++ src/test/java/PingResourceTest.java
@@ -8,6 +8,11 @@ public class PingResourceTest {
 
   @Test
   public void testPing() {
+    // TODO: currently the config property VERSION is null in our test
+    // somehow the microprofile-config.properties is not evaluated.
+    // Even if we override it with System.setProperty("VERSION", "0.0.1")
+    // it returns null. Maybe its because we are not starting a embedded
+    // payara server? Do we need a jee testing framework?
     String responseRegex = "^Hello from Java EE(.+)";
     PingResource pingRequest = new PingResource();
     String response = pingRequest.ping();
